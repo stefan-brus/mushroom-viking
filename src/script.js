a32c2084ed9bbc4a2fef13817e80c5b6daa3b566
Stefan Brus <stefan1brus@gmail.com>
2014-01-22 13:49:52 +0100

    Create basic file structure
    Fixes #4.
    Splits the HTML, CSS and JavaScript into separate files.

---
+++ src/script.js
@@ -0,0 +1,5 @@
+function pick () {
+    var mushrooms = parseInt(document.getElementById('mushrooms').innerHTML);
+    mushrooms++;
+    document.getElementById('mushrooms').innerHTML = mushrooms;
+}
