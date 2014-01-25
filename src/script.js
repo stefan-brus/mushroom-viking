b8b749b6339475e5effd61f6d22043c0334c0c5a
Stefan Brus <stefan1brus@gmail.com>
2014-01-25 23:39:09 +0100

    Limit "auto-clicks" per second
    Fixes #3.
    The auto-clickers now stop auto-clicking after a number of auto-clicks per
    second. When this limit is hit, the number of mushrooms per auto-click is
    increased instead.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
+var AUTO_CLICKS_PER_SECOND = 10;
+
 var CLICKER_PRICE = 10;
 
 var mushrooms = 0;
+
+var no_clickers = 0;
+
+var auto_click_factor = 0;
 
 window.onload = initGame;
 
@@ -10,21 +16,33 @@ function initGame () {
 }
 
 function updateMushrooms() {
-    document.getElementById('mushrooms').innerHTML = mushrooms;
+    document.getElementById('mushrooms').innerHTML = parseInt(mushrooms);
 }
 
 function updateClickerPrices() {
     document.getElementById('clickerPrice').innerHTML = CLICKER_PRICE;
 }
 
-function pick () {
+function pick (isClick) {
     mushrooms++;
+
+    if (!isClick) {
+        mushrooms = mushrooms + auto_click_factor;
+    }
+
     updateMushrooms();
 }
 
 function addClicker() {
     if ( mushrooms >= CLICKER_PRICE ) {
-        window.setInterval(pick, 1000);
+        if (no_clickers < AUTO_CLICKS_PER_SECOND) {
+            no_clickers++;
+            window.setInterval(function() { pick(false); }, 1000 / no_clickers);
+        }
+        else {
+            auto_click_factor = auto_click_factor + (1 / AUTO_CLICKS_PER_SECOND);
+        }
+
         mushrooms = mushrooms - CLICKER_PRICE;
         updateMushrooms();
     }
