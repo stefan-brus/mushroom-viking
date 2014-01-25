00d6771b123fb3e12a9f30080e469f65ca5def40
Stefan Brus <stefan1brus@gmail.com>
2014-01-25 23:40:01 +0100

    Add reset button
    Fixes #6.
    Adds a button that resets the mushroom counter, and data related to
    auto-clickers.
    
    Conflicts:
    	src/script.js

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ var no_clickers = 0;
 
 var auto_click_factor = 0;
 
+var clicker_event_ids = new Array();
+
 window.onload = initGame;
 
 function initGame () {
@@ -37,7 +39,7 @@ function addClicker() {
     if ( mushrooms >= CLICKER_PRICE ) {
         if (no_clickers < AUTO_CLICKS_PER_SECOND) {
             no_clickers++;
-            window.setInterval(function() { pick(false); }, 1000 / no_clickers);
+            clicker_event_ids.push(window.setInterval(function() { pick(false); }, 1000 / no_clickers));
         }
         else {
             auto_click_factor = auto_click_factor + (1 / AUTO_CLICKS_PER_SECOND);
@@ -46,4 +48,15 @@ function addClicker() {
         mushrooms = mushrooms - CLICKER_PRICE;
         updateMushrooms();
     }
+}
+
+function reset() {
+    mushrooms = 0;
+    no_clickers = 0;
+    auto_click_factor = 0;
+	clicker_event_ids.forEach(function(entry) {
+		window.clearInterval(entry);
+	});
+	clicker_event_ids.length = 0;
+    updateMushrooms();
 }
