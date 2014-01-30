ae30a6744ed76a5965e52c56ff23cb3edca014f2
Stefan Brus <stefan1brus@gmail.com>
2014-01-30 01:08:19 +0100

    Rework clicker logic
    Fixes #16.
    Makes use of the new object oriented clicker approach.

--- src/script.js
+++ src/script.js
@@ -3,17 +3,13 @@ var AUTO_CLICKS_PER_SECOND = 10;
 // This magic number has been decided by thorough research and ruthless testing
 var PRICE_INCREASE_RATE = 1.218;
 
-var ORIG_CLICKER_PRICE = 10;
-
 var SAVE_COOKIE = 'saveGame';
 
 var mushrooms = 0;
 
+var clickers = new Array();
+
 var mps = 0;
-
-var clicker_price = ORIG_CLICKER_PRICE;
-
-var no_clickers = 0;
 
 var auto_click_factor = 0;
 
@@ -28,30 +24,61 @@ function initGame () {
     $('#picker').click(function() {
         pick(true);
     });
-    
+
     $('#reset').click(function() {
         reset();
     });
-    
+
     $('#save').click(function() {
         save();
-    });
-
-    $('li').click(function() {
-        addClicker();
     });
 
     $('.box').equalHeights();
 
     $.cookie.json = true;
 
+    initClickers();
+
+    var has_clicker = false;
+
     load();
+
+    for(var id in clickers) {
+        var button_id = '#';
+        button_id += id;
+        
+        $(button_id).click(function() {
+            addClicker(id);
+        });
+        
+        if(clickers[id].count > 0) {
+            has_clicker = true;
+        }
+    }
 
     updateMushrooms();
     updateClickerPrices();
     updateStatistics();
-    
+
     startAutoSaver();
+
+    if (has_clicker) {
+        startAutoClicker();
+    }
+}
+
+function initClickers() {
+    var phantom_hand = new Clicker();
+    phantom_hand.orig_price = 10;
+    phantom_hand.cur_price = 10;
+    phantom_hand.added_mps = 1;
+    phantom_hand.count = 0;
+
+    // We stray a bit from our variable_name_convention here
+    // This is so that the keys in this array can be used for DOM ids
+    clickers = {
+        'phantom-hand': phantom_hand
+    };
 }
 
 function updateMushrooms() {
@@ -59,7 +86,13 @@ function updateMushrooms() {
 }
 
 function updateClickerPrices() {
-    $('#phantom-hand-price').text(Math.round(clicker_price));
+    for(var id in clickers) {
+        var price_id = '#';
+        price_id += id;
+        price_id += '-price';
+        
+        $(price_id).text(Math.round(clickers[id].cur_price));
+    }
 }
 
 function updateStatistics() {
@@ -73,12 +106,26 @@ function updateMps() {
 }
 
 function updateClickerStatistics() {
-    $('#phantom-hand-count').text(parseInt(mps));
-    $('#phantom-hand-mps').text(mps.toFixed(2));
+    for(var id in clickers) {
+        var count_id = '#';
+        count_id += id;
+        count_id += '-count';
+
+        var mps_id = '#';
+        mps_id += id;
+        mps_id += '-mps';
+
+        $(count_id).text(parseInt(clickers[id].count + auto_click_factor * 10));
+        $(mps_id).text((clickers[id].total_mps() + auto_click_factor * 10).toFixed(2));
+    }
 }
 
 function calculateMps() {
-    mps = no_clickers * 1 + (auto_click_factor * 10);
+    var new_mps = 0;
+    for(var id in clickers) {
+        new_mps += clickers[id].count * clickers[id].added_mps;
+    }
+    mps = new_mps + auto_click_factor * 10;
 }
 
 function startAutoSaver() {
@@ -95,10 +142,12 @@ function pick (isClick) {
     updateMushrooms();
 }
 
-function addClicker() {
-    if ( mushrooms >= clicker_price ) {
-        if (no_clickers < AUTO_CLICKS_PER_SECOND) {
-            no_clickers++;
+function addClicker(id) {
+    var price = clickers[id].cur_price;
+
+    if ( mushrooms >= price ) {
+        if (clickers[id].count < AUTO_CLICKS_PER_SECOND) {
+            clickers[id].count++;
             resetClickerEvent();
             startAutoClicker();
         }
@@ -106,20 +155,16 @@ function addClicker() {
             auto_click_factor = auto_click_factor + (1 / AUTO_CLICKS_PER_SECOND);
         }
 
-        mushrooms = mushrooms - clicker_price;
-        increasePrice();
+        mushrooms = mushrooms - price;
+        clickers[id].cur_price *= PRICE_INCREASE_RATE;
         updateMushrooms();
         updateClickerPrices();
         updateStatistics();
     }
 }
 
-function increasePrice() {
-    clicker_price = clicker_price * PRICE_INCREASE_RATE;
-}
-
 function startAutoClicker() {
-    clicker_event_id = window.setInterval(function() { pick(false); }, 1000 / no_clickers);
+    clicker_event_id = window.setInterval(function() { pick(false); }, 1000 / clickers['phantom-hand'].count);
 }
 
 function resetClickerEvent() {
@@ -131,9 +176,8 @@ function resetClickerEvent() {
 
 function reset() {
     mushrooms = 0;
-    no_clickers = 0;
-    clicker_price = ORIG_CLICKER_PRICE;
     auto_click_factor = 0;
+    initClickers();
     resetClickerEvent();
     updateMushrooms();
     updateClickerPrices();
@@ -148,14 +192,15 @@ function load() {
     if ($.cookie(SAVE_COOKIE)) {
         var jsonState = $.cookie(SAVE_COOKIE);
         mushrooms = jsonState['mushrooms'];
-        no_clickers = jsonState['no_clickers'];
-        clicker_price = jsonState['clicker_price'];
         auto_click_factor = jsonState['auto_click_factor'];
-        clicker_event_id = jsonState['clicker_event_id'];
 
-        if (no_clickers > 0) {
-            resetClickerEvent();
-            startAutoClicker();
+        if (typeof jsonState['clickers'] != 'undefined') {
+            var new_clickers = jsonState['clickers'];
+
+            for (var id in new_clickers) {
+                clickers[id].count = new_clickers[id].count;
+                clickers[id].cur_price = new_clickers[id].cur_price;
+            }
         }
     }
 }
@@ -163,10 +208,8 @@ function load() {
 function jsonifyState() {
     var result = {
         'mushrooms': mushrooms,
-        'no_clickers': no_clickers,
-        'clicker_price': clicker_price,
         'auto_click_factor': auto_click_factor,
-        'clicker_event_id': clicker_event_id
+        'clickers': clickers,
        };
     return result;
 }
